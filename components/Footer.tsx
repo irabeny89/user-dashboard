23e55e8acbec89cc198ca51101339386f6411cd0
Ernest Irabor <irabeny89@gmail.com>
2021-07-12 15:01:09 +0100

    Header and footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <footer className="bg-secondary text-center text-white">
+    Made by Ernest
+  </footer>
+);
+
+export default Footer;
---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+const Header = () => (
+  <header>
+    <h1 className="text-center bg-info text-dark py-2 mb-0">Dashboard</h1>
+  </header>
+);
+
+export default Header;
