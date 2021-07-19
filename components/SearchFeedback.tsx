6899714c215f1eb4d08222315a23fe207430aca6
Ernest Irabor <irabeny89@gmail.com>
2021-07-19 11:18:30 +0100

    feedback component initial commit

---
+++ components/SearchFeedback.tsx
@@ -0,0 +1,40 @@
+import { CSSProperties, MutableRefObject } from "react";
+import { Badge } from "react-bootstrap";
+import { constantVariable } from "../config";
+import type { UserType } from "../interfaces";
+
+type SearchFeedbackProps = { latestUsers: MutableRefObject<UserType[]> };
+
+const SearchFeedback = ({ latestUsers }: SearchFeedbackProps) => {
+  const {
+    ALL_USERS_ICON_BG_COLOR,
+    FEMALE_USERS_ICON_BG_COLOR,
+    MALE_USERS_ICON_BG_COLOR,
+  } = constantVariable;
+  const allUsersNumber = latestUsers.current.length;
+  const maleUsersNumber = latestUsers.current.filter(
+    ({ gender }) => gender == "male"
+  );
+  const femaleUsersNumber = latestUsers.current.filter(
+    ({ gender }) => gender == "female"
+  );
+  const allUsersStyle: CSSProperties = {
+    backgroundColor: ALL_USERS_ICON_BG_COLOR,
+  };
+  const maleUsersStyle: CSSProperties = {
+    backgroundColor: MALE_USERS_ICON_BG_COLOR,
+  };
+  const femaleUsersStyle: CSSProperties = {
+    backgroundColor: FEMALE_USERS_ICON_BG_COLOR,
+  };
+  return (
+    // <small>{`Found: ${allUsersNumber} | Male: ${maleUsersNumber} Female: ${femaleUsersNumber}`}</small>
+    <div>
+      Found: <Badge style={allUsersStyle}>{allUsersNumber}</Badge> | Male:{" "}
+      <Badge style={maleUsersStyle}>{maleUsersNumber}</Badge> | Female:{" "}
+      <Badge style={femaleUsersStyle}>{femaleUsersNumber}</Badge>
+    </div>
+  );
+};
+
+export default SearchFeedback;
